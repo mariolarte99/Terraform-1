85b44a836d8c30e190d29e5634b10a2a5a0f882f
mariolarte99 <61019362+mariolarte99@users.noreply.github.com>
2020-04-14 09:00:20 -0500

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "username" {
   description = "Enter your Skytap username, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 }
-variable "api_token " {
+variable "api_token" {
   description = "Enter your IBM Cloud API token, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 }
